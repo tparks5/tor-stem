62a795d05898728f434d50aa4a9f25c13603b398
Tyler Parks <tparks5@mail.csuchico.edu>
2017-05-04 21:11:53 -0700

    removed consensus validation unit test ddependence on network docs from live tor network, performed git-fu to purge all traces of live network docs from history

--- test/unit/descriptor/networkstatus/document_v3.py
+++ test/unit/descriptor/networkstatus/document_v3.py
@@ -1288,7 +1288,7 @@ DnN5aFtYKiTc19qIC7Nmo+afPdDEf0MlJvEOP5EWl3w=
     empty, and rejected with exception if signatures are invalid.
     """
 
-    with open(get_resource('cached-consensus-sig-validation'), 'rb') as document_file, open(get_resource('cached-certs-sig-validation'), 'rb') as key_file:
+    with open(get_resource('cached-consensus'), 'rb') as document_file, open(get_resource('cached-certs'), 'rb') as key_file:
       key_certs = stem.descriptor.networkstatus._parse_file_key_certs(key_file, validate = True)
       content = document_file.read()
 
@@ -1316,7 +1316,7 @@ DnN5aFtYKiTc19qIC7Nmo+afPdDEf0MlJvEOP5EWl3w=
     later time.
     """
 
-    with open(get_resource('cached-consensus-sig-validation'), 'rb') as document_file, open(get_resource('cached-certs-sig-validation'), 'rb') as key_file:
+    with open(get_resource('cached-consensus'), 'rb') as document_file, open(get_resource('cached-certs'), 'rb') as key_file:
       key_certs = stem.descriptor.networkstatus._parse_file_key_certs(key_file, validate = True)
       content = document_file.read()
 
@@ -1335,7 +1335,7 @@ DnN5aFtYKiTc19qIC7Nmo+afPdDEf0MlJvEOP5EWl3w=
     Test that a damaged signature is rejected with an exception
     """
 
-    with open(get_resource('cached-consensus-sig-validation'), 'rb') as document_file, open(get_resource('cached-certs-sig-validation'), 'rb') as key_file:
+    with open(get_resource('cached-consensus'), 'rb') as document_file, open(get_resource('cached-certs'), 'rb') as key_file:
       key_certs = stem.descriptor.networkstatus._parse_file_key_certs(key_file, validate = True)
       content = document_file.read()
 
@@ -1359,7 +1359,7 @@ DnN5aFtYKiTc19qIC7Nmo+afPdDEf0MlJvEOP5EWl3w=
     from cryptography.hazmat.primitives.asymmetric import rsa
     from cryptography.hazmat.primitives import serialization
 
-    with open(get_resource('cached-consensus-sig-validation'), 'rb') as document_file, open(get_resource('cached-certs-sig-validation'), 'rb') as key_file:
+    with open(get_resource('cached-consensus'), 'rb') as document_file, open(get_resource('cached-certs'), 'rb') as key_file:
       key_certs = stem.descriptor.networkstatus._parse_file_key_certs(key_file, validate = True)
       content = document_file.read()
 
